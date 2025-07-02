ffba60ac046801fd7a66b729787de07b7a195f5f
uncleyellowdk@ratraco.vn <uncleyellowdk@ratraco.vn>
2025-07-02 16:32:44 +0700

    load lại data

--- src/app/modules/admin/example/example.component.ts
+++ src/app/modules/admin/example/example.component.ts
@@ -188,7 +188,7 @@ export class ExampleComponent
     
         this.http.post(`${environment.apiUrl}/${this.selectedTable}/write`, payload).subscribe(response => {
             console.log("Dữ liệu cập nhật lên Google Sheets:", response);
-            // this.loadData();
+            this.loadData();
             // Không cần loadData ở đây, vì việc này có thể gây gián đoạn nhập liệu.
             // Dữ liệu sẽ được cập nhật khi người dùng hoàn thành chỉnh sửa hoặc làm mới trang.
         }, error => {
